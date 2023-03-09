5495b5206711b9324e34c911005a189ea1d39bdd
Albina Gilmutdinova <inovaras@mail.ru>
2023-03-09 19:34:20 +0500

    fixed test_urls

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,9 +1,11 @@
+from http import HTTPStatus
 from django.test import TestCase, Client
 
 from ..models import Group, Post, User
 
 
-USERNAME = 'author'
+USERNAME = 'user'
+AUTHOR = 'author'
 SLUG = 'some_slug'
 TEXT = 'some text'
 
@@ -13,9 +15,10 @@ class PostURLTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create_user(username=USERNAME)
+        cls.author = User.objects.create_user(username=AUTHOR)
         cls.group = Group.objects.create(slug=SLUG)
         cls.post = Post.objects.create(
-            text=TEXT, author=cls.user, group=cls.group
+            text=TEXT, author=cls.author, group=cls.group
         )
         cls.access_url_address_map = {
             '/': 'all',
@@ -38,12 +41,15 @@ class PostURLTests(TestCase):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
 
+        self.author_client = Client()
+        self.author_client.force_login(self.author)
+
     def test_urls_uses_correct_template(self):
         """URL-адрес использует соответствующий шаблон."""
 
         for address, template in self.template_url_name.items():
             with self.subTest(address=address):
-                response = self.authorized_client.get(address)
+                response = self.author_client.get(address)
                 self.assertTemplateUsed(response, template)
 
     def test_url_access_for_authorized_only(self):
@@ -52,20 +58,20 @@ class PostURLTests(TestCase):
             if access == 'authorized':
                 with self.subTest(address=address):
                     response = self.authorized_client.get(address)
-                    self.assertEqual(response.status_code, 200)
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_url_access_for_all(self):
         """Страницы, доступные всем пользователям"""
         for address, access, in self.access_url_address_map.items():
             if access == 'all':
                 with self.subTest(address=address):
-                    response = self.authorized_client.get(address)
-                    self.assertEqual(response.status_code, 200)
+                    response = self.guest_client.get(address)
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_url_access_for_author_only(self):
         """Страницы, доступные только автору"""
         for address, access, in self.access_url_address_map.items():
             if access == 'author':
                 with self.subTest(address=address):
-                    response = self.authorized_client.get(address)
-                    self.assertEqual(response.status_code, 200)
+                    response = self.author_client.get(address)
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
